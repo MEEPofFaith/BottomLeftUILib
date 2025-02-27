516234509bd62a5ec4da66384631c9e09ae20bce
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2025-02-27 00:49:26 -0800

    Fix swap button folding on first press

--- src/blui/BLVars.java
+++ src/blui/BLVars.java
@@ -3,7 +3,7 @@ package blui;
 import arc.*;
 
 public class BLVars{
-    public static float pressTimer = 30f;
+    public static float pressTimer = 0;
     public static float longPress = 30;
     public static float iconSize = 40f, buttonSize = 24f, sliderWidth = 140f, fieldWidth = 80f;
 }
